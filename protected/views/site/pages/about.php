f01d3c93043ebcdf750b3ed43c47439023aa6477
José Mario López <jmlb92@gmail.com>
2013-06-16 22:49:28 -0600

    Updated About page

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -8,5 +8,24 @@ $this->breadcrumbs=array(
 ?>
 <h1>About</h1>
 
+<p>HMS was created under the need of having a simple, unified medical system for all of our users. We aim to solve
+the problem of misplacing patient information once and for all. With this system participating Doctors, Hospitals
+and Patients can easily locate a patient's medical history. This makes the process of starting tratments for these 
+patients dramatically faster.</p>
+
+<h3>All Your History, Here.</h3>
+<p>Quick access to your past medical history and safeguard for all your future checkups. Helping
+your Doctors help you.</p>
+
+<h3>Doctors</h3>
+<p>If you are a Doctor interested in becoming a participant in our system please contact our administating panel 
+to recieve approval for your profile.</p>
+
+<h3>Patients</h3>
+<p>Any person is free to sign up as a patient to start using our services.</p>
+
+<h3>Feedback</h3>
+<p>Have any feedback, comments or suggestions? We'd love to hear you out. Hit us up: honduras@medicalcenter.com</p>
+
 <p>This is a "static" page. You may change the content of this page
 by updating the file <code><?php echo __FILE__; ?></code>.</p>
