bbaa4984a9bfd2472160610f2ee0d580f9a4a787
José Mario López <jmlb92@gmail.com>
2013-06-17 05:38:21 -0600

    Changes to Admin Panel
    Botonos bonitos

--- protected/views/site/manage.php
+++ protected/views/site/manage.php
@@ -1,16 +1,21 @@
-<?php $this->widget('bootstrap.widgets.TbButton', array(
+<div class="hero-unit">
+	<h2>Need a Doctor?</h2>
+  <?php $this->widget('bootstrap.widgets.TbButton', array(
     'label'=>'Create a Doctor',
     'type'=>'primary', // null, 'primary', 'info', 'success', 'warning', 'danger' or 'inverse'
     'size'=>'large', // null, 'large', 'small' or 'mini'
     'url'=>Yii::app()->createUrl('doctors/create'),
 ));
  ?>
-
-<br><br>
-
-<?php $this->widget('bootstrap.widgets.TbButton', array(
+</div>
+<br>
+<div class="hero-unit">
+	<h2>Need a Patient?</h2>
+  <?php $this->widget('bootstrap.widgets.TbButton', array(
     'label'=>'Create a Patient',
     'type'=>'primary', // null, 'primary', 'info', 'success', 'warning', 'danger' or 'inverse'
     'size'=>'large', // null, 'large', 'small' or 'mini'
     'url'=>Yii::app()->createUrl('patient/create'),
 )); ?>
+</div>
+
